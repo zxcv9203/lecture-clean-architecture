abe8258d21fdfcfb1b62f6de0227cd11ca9ea751
yongckim <zxcv9203@naver.com>
2024-09-29 11:23:40 +0900

    :tada: init : 프로젝트 초기화

---
+++ src/main/kotlin/org/example/lecturecleanarchitecture/LectureCleanArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package org.example.lecturecleanarchitecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LectureCleanArchitectureApplication
+
+fun main(args: Array<String>) {
+    runApplication<LectureCleanArchitectureApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/lecturecleanarchitecture/LectureCleanArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.lecturecleanarchitecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LectureCleanArchitectureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
